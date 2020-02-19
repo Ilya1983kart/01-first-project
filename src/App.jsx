c65b3954e23e714495e7db4f40afb7d4109dd365
KartachovIA <ikartachov@yandex.ru>
2020-02-19 12:20:43 +0300

    Работаем с методом map

--- src/App.jsx
+++ src/App.jsx
@@ -22,11 +22,11 @@ const App = () => {
                 <Header />
                 <Navbar />
                 <div className='appWrapperContent'>
-                    <Route path='/dialogs' component={Dialogs} />
-                    <Route path='/profile' component={Profile} />
-                    <Route path='/news' component={News} />
-                    <Route path='/music' component={Music} />
-                    <Route path='/settings' component={Settings} />
+                    <Route  path='/dialogs' component={Dialogs} />
+                    <Route  path='/profile' component={Profile} />
+                    <Route  path='/news' component={News} />
+                    <Route  path='/music' component={Music} />
+                    <Route  path='/settings' component={Settings} />
                 </div>
             </div>
         </div>
